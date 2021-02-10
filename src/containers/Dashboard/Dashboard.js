b8a91f59eb4a2206530232e7df74b21c60c30ea1
acavalls <alejandro.canosa@bsc.es>
2021-02-10 14:09:51 +0000

    Dashboard (Home) upgrade: Adding responsive card-grid + animations

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import classes from './Dashboard.module.css';
 import * as _ from 'lodash';
 
 class Home extends Component {
@@ -7,58 +6,46 @@ class Home extends Component {
     return (
       <React.Fragment>
         <div class="container-fluid">
-
           <div class="row">
-            <div class="col-lg-12 text-left mt-5 mb-5 pl-5">
-              <h1> How does the catalogue work?</h1>
+            <div class="col-lg-12 text-left mt-5 mb-4 pl-5">
+              <h1> Data Catalogue </h1>
             </div>
-
-            <div class="col-lg-6 pl-5">
+            <div class="col-lg-12 pb-3 pl-5">
               <p> Data catalogue leverages the power of Overture data management stack.</p>
               <p> The portal engine allows to find data using metadata as a filter. </p>
               <p> It will also have direct access to Virtual Research Environment and Cavatica for data analysis. </p>
               <p> Users will have granted access to different datasets depending on their privileges. </p>
             </div>
-
-            <div class="col-lg-6 text-center">
-              <h1> iPC Catalogue </h1>
-            </div>
-
           </div>
-
-          <div class="row mt-3 pl-5">
-            <div className={classes.boxes}>
-              <p> Search files by filtering from stored metadata in an user friendly interface. </p>
-              <a href="/filerepository" exact className={classes.linkStyle}> Take me there! </a>
+          
+          <div class="row card-list">
+            <div class="col-sm-6 card-list--item pl-3 pr-3">
+              <div class="card card-style card-list--content">
+                <div class="card-body card-style__content">
+                  <h5 class="card-title card-style__content--title"> Search Area </h5>
+                  <p class="card-text card-style__content--para">
+                    Search files by filtering from stored metadata in an user friendly interface. 
+                  </p>
+                  <a href="/filerepository" class="card-style__content--btn card-style__content--btn-blue card-style__content--btn-animated"> Search </a>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div class="row mt-3 pl-5">
-            <div className={classes.boxes}>
-              <p> Login first to get granted privileges and accessing your selected files. </p>
-              <a href="https://inb.bsc.es/auth/realms/IPC/protocol/openid-connect/auth?state=5484f2ae69fa95f00286affff7c7514c&response_type=code&approval_prompt=auto&redirect_uri=http%3A%2F%2Fvre.ipc-project.bsc.es%2Fopenvre%2Fapplib%2FloginToken.php&client_id=Ipc-vre"
-                 target="_blank"
-                 className={classes.linkStyle}> Login
-              </a>
+            <div class="col-sm-6 card-list--item pl-3 pr-3">
+              <div class="card card-style card-list--content">
+                <div class="card-body card-style__content">
+                  <h5 class="card-title card-style__content--title"> Data Management Area </h5>
+                  <p class="card-text card-style__content--para">
+                    Inspect your datasets, and expose them to the analysis platforms (VRE, Cavatica)
+                  </p>
+                  <a href="/explore" class="card-style__content--btn card-style__content--btn-blue card-style__content--btn-animated"> Manage </a>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div class="row mt-3 pl-5">
-            <div className={classes.boxes}>
-              <p> Or directly visualize statistics in an interactive way. </p>
-              <a href="/explore" exact className={classes.linkStyle}> Go and explore your data </a>
-            </div>
-          </div>
-
+          </div>  
         </div>
-
       </React.Fragment>
     )
   }
 }
 
 export default Home;
-
-
-
-
