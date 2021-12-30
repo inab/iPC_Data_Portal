e0a54c83854bc5a8a15b5ad40e352175b3c15a7c
acavalls <alejandro.canosa@bsc.es>
2021-12-30 11:59:14 +0000

    Use sessionStorage persistor (Redux) for cart selections

--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+//import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session';
 import cartReducer from './cart/cart.reducer';
 import userSelectionsReducer from './userSelections/userSelections.reducer';
 
 const persistConfig = {
   key: 'root',
-  storage,
+  storage: storageSession,
   whitelist: ['cart']
 };
 
