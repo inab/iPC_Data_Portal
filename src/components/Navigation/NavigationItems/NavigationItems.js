c0dd2758c3d702a5e66466f9d32c88bdcd14ae30
acavalls <alejandro.canosa@bsc.es>
2022-01-04 12:11:57 +0100

    Logout link: Setting AuthN server prefix via env-var

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const logout = "https://inb.bsc.es/auth/realms/IPC/protocol/openid-connect/logout?redirect_uri=" + process.env.REACT_APP_URL;
+const logout = process.env.REACT_APP_AUTH_URL + "/realms/IPC/protocol/openid-connect/logout?redirect_uri=" + process.env.REACT_APP_URL;
 
 const navigationItems = () => (
     <ul class="navbar-nav ml-auto">
