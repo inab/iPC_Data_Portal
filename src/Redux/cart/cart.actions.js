484e8dd1faa469c88823d3e2fd648e17e2ce39ac
acavalls <alejandro.canosa@bsc.es>
2022-06-22 11:35:18 +0200

    Refactoring (III): Redux & Requests (cart-actions) -> Abstracting implementation details from dispatcher

--- src/Redux/cart/cart.actions.js
+++ src/Redux/cart/cart.actions.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import axios from 'axios';
+import { getUserDatasetPermissions, generateCartStore } from '../../Services/ManagePermissions';
 
 export const addItemToWhitelist = item => ({
   type: CartActionTypes.ADD_ITEM_TO_WHITELIST,
@@ -16,38 +16,11 @@ export const removeItem = item => ({
   payload: item
 });
 
-export const addItem = (list) => {
-  return dispatch => {
-    const access_token = localStorage.getItem("react-token");
-    const { REACT_APP_PERMISSIONS_URL } = process.env;
-    axios({
-      method: 'get',
-      url: `${REACT_APP_PERMISSIONS_URL}/me/permissions?format=PLAIN`, 
-      headers: {
-        Authorization: "Bearer " + access_token
-      }
-    }).then(response => {
-      let allowedIds;
-      let allowedItems;
-      let restrictedItems;
-
-      if(response.data.length > 0){
-        allowedIds = response.data.map(visa => JSON.parse(visa).ga4gh_visa_v1.value.split(":").pop())
-        allowedItems = list.filter((item) => allowedIds.includes(item["file_ID"]) || item["access"] == "public");
-        restrictedItems = list.filter((item) => !allowedIds.includes(item["file_ID"]) && item["access"] != "public");
-	console.log(allowedItems)
-        allowedItems = allowedItems.reduce((a, b) => a.concat(b), []);
-      } else {
-        allowedItems = list.filter((item) => item["access"] == 'public');
-        restrictedItems = list.filter((item) => item["access"] != 'public');
-      }
-
-      dispatch(addItemToWhitelist(allowedItems));
-      dispatch(addItemToBlacklist(restrictedItems));
-
-    })
-    .catch(error => {
-      throw error;
-    }); 
+export const addItem = (datasetList) => {
+  return async (dispatch) => {
+      const userDatasetPermissions = await getUserDatasetPermissions();
+      const cartStore = generateCartStore(datasetList, userDatasetPermissions);
+      dispatch(addItemToWhitelist(cartStore[0]));
+      dispatch(addItemToBlacklist(cartStore[1]));
   }
 };
