a2e7a1368ce6fc0550dfa61077d1190247bd93a8
acavalls <alejandro.canosa@bsc.es>
2022-06-25 10:43:25 +0200

    Refactoring (V): Redux & Requests (userSelections - Metadata for datasets selected for analysis) -> Adding ManageMetadata service

---
+++ src/Services/ManageMetadata.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+// IPC-OUTBOX-API: Get metadata from elements selected for analysis
+const getDatasetsLocators = async () => {
+    const { REACT_APP_OUTBOX_URL } = process.env
+
+    const response = await axios({
+        method: 'get',
+        url: `${REACT_APP_OUTBOX_URL}/v1/metadata`,
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("react-token")
+        }
+    })
+
+    return response.data
+}
+
+const getDatasetsMetadata = async (datasetsLocators) => {
+    const { REACT_APP_ES_URL } = process.env
+
+    let objects = await Promise.all(datasetsLocators.map(async (locator) => {
+        let metadata = await axios({
+            method: 'get',
+            url: `${REACT_APP_ES_URL}/${locator.metadata.es_index}/_search?pretty=true&size=10000&q=file_ID:${locator._id}`,
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("react-token")
+            }
+        })
+        return {
+            'metadata': metadata.data.hits.hits[0]._source,
+            'analysis': locator.metadata.analysis
+        }
+    }));
+
+    return objects
+}
+
+const generateSelectionsStore = (datasetsMetadata) => {
+
+    let store = [];
+    let vreStore = [];
+    let cavaticaStore = [];
+
+    datasetsMetadata.map(dataset => {
+        if (dataset.analysis === "vre") {
+            vreStore.push(dataset.metadata)
+        } else cavaticaStore.push(dataset.metadata)
+    })
+
+    store.push(vreStore)
+    store.push(cavaticaStore)
+
+    return store
+}
+
+export { getDatasetsLocators, getDatasetsMetadata, generateSelectionsStore }
