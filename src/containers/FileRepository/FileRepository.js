bdd47b364debdcf37c1dd82063c6d980913a678f
acavalls <alejandro.canosa@bsc.es>
2022-06-28 07:59:11 +0200

    Cleaning code: Removing unnecessary componentDidMount call (file repository)

--- src/containers/FileRepository/FileRepository.js
+++ src/containers/FileRepository/FileRepository.js
@@ -2,35 +2,12 @@ import React, { Component } from 'react';
 import Search from '../Search/Search';
 
 class FileRepository extends Component {
-    constructor(props){
-      super(props)
-
-      this.state = {
-        token : null,
-        refresh : null 
-      }
-  
-    }
-
-  componentDidMount () {
-    const token = localStorage.getItem("react-token");
-    const refresh = localStorage.getItem("react-refresh-token");  
-    this.setState({
-      token: token,
-      refresh: refresh
-    })
-  }
 
   render() {
-
-    console.log("Token: ", this.state.token);
-    console.log("Refresh token: ", this.state.refresh);
 
     return (
 
       <React.Fragment>
-        
-        <br/>
         
         <div class="container-fluid">
             <div class="row">
