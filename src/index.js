2b7fab4e53aa8e5955011c0f50792cddde916c1c
acavalls <alejandro.canosa@bsc.es>
2022-07-01 16:48:07 +0200

    Refactoring (VII b.): Abstracting the implementation login for AuthN/Z (promisifying non-ES6 Keycloak responses)

--- src/index.js
+++ src/index.js
@@ -2,27 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as Keycloak from 'keycloak-js';
+import { getAccessToken, getRefreshToken, getUserRoles, userAuth } from './Services/ManageAuth';
 import { Provider } from 'react-redux';
 import { store, persistor } from './Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import FetchUserSelections from './Layout/FetchUserSelections/FetchUserSelections';
 
-const { REACT_APP_AUTH_URL } = process.env;
+const initApp = async () => {
+    const isAuthenticated = await userAuth();
 
-let initOptions = {
-    url: REACT_APP_AUTH_URL, realm: 'IPC', clientId: 'ipc-react-portal', onLoad: 'login-required'
-}
+    if(!isAuthenticated) window.location.reload();
 
-let keycloak = Keycloak(initOptions);
-
-keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
-
-    if (!auth) {
-        window.location.reload();
+    if(getUserRoles() === 'admin') {
+        localStorage.setItem("role", 'admin');
     } else {
-        console.info("Authenticated");
+        localStorage.setItem("role", 'user');
     }
+
+    localStorage.setItem("react-token", getAccessToken());
+    localStorage.setItem("react-refresh-token", await getRefreshToken());
 
     const app = (
         <Provider store={store}>
@@ -35,35 +33,8 @@ keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
             </BrowserRouter>
         </Provider>
     );
-
-    // Check user roles
-
-    var roles = [];
-
-    if(keycloak["tokenParsed"]["realm_access"]["roles"].includes("app-admin")){
-        localStorage.setItem("role", 'admin');
-    } else {
-        localStorage.setItem("role", 'user');
-    }
-
-    localStorage.setItem("react-token", keycloak.token);
-    localStorage.setItem("react-refresh-token", keycloak.refreshToken);
-
-    setTimeout(() => {
-        keycloak.updateToken(70).success((refreshed) => {
-            if (refreshed) {
-                console.debug('Token refreshed' + refreshed);
-            } else {
-                console.warn('Token not refreshed, valid for '
-                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-            }
-        }).error(() => {
-            console.error('Failed to refresh token');
-        });
-    }, 60000)
-
+    
     ReactDOM.render(app, document.getElementById('root'));
+}
 
-}).error(() => {
-    console.error("Authenticated Failed");
-});
+initApp();
