a9655cf5af1ca117304a08b0c49b07f0d5acf487
acavalls <alejandro.canosa@bsc.es>
2022-10-07 08:01:41 +0200

    Services (ManageAuth): Adding request for token exchange (BSC access token -> EGA access token)

--- src/Services/ManageAuth.js
+++ src/Services/ManageAuth.js
@@ -1,4 +1,5 @@
 import * as Keycloak from 'keycloak-js';
+import axios from 'axios';
 
 const { REACT_APP_AUTH_URL } = process.env;
 
@@ -28,4 +29,35 @@ const getAccessToken = () => {
     return keycloak.token
 };
 
-export { getAccessToken, getRefreshToken, getUserRoles, userAuth }
+const getUserInfo = async () => {
+    return await axios({
+        method: 'get',
+        url: `${REACT_APP_AUTH_URL}/realms/IPC/protocol/openid-connect/userinfo`,
+        headers: {
+          Authorization: "Bearer " + keycloak.token
+        }
+      })
+}
+
+const getEGAAccessToken = async () => {
+    const params = {
+        client_id: 'ipc-rest',
+        requested_issuer: 'ega',
+        grant_type: 'urn:ietf:params:oauth:grant-type:token-exchange',
+        requested_token_type : 'urn:ietf:params:oauth:token-type:access_token',
+        subject_token : keycloak.token
+    };
+      
+    const data = Object.keys(params).map((key) => `${key}=${encodeURIComponent(params[key])}`).join('&');
+    
+    return await axios({
+        method: 'post',
+        url: `${REACT_APP_AUTH_URL}/realms/IPC/protocol/openid-connect/token`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data
+    })    
+}
+
+export { getAccessToken, getRefreshToken, getUserRoles, userAuth, getUserInfo, getEGAAccessToken }
